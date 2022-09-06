a7a6dba4bfff5a1973b685c03c298afe5647cf17
Nick Qurle <wrl.snk@gmail.com>
2022-09-06 03:19:52 +0300

    it fucking works

--- code.js
+++ code.js
@@ -10,65 +10,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 // Constants
 const confirmMsgs = ["Done!", "You got it!", "Aye!", "Is that all?", "My job here is done.", "Gotcha!", "It wasn't hard.", "Got it! What's next?"];
-const renameMsgs = ["Renamed", "Affected", "Made it with", "No numbered", "Cleared"];
-const idleMsgs = ["No numbers, already", "I see no layers with numbers", "Any default numbers? I can't see it", "Nothing to do, your layers are great"];
-const regex = /^\D\w+(?= \d+)/g;
-// Affected layers
-const dict = [
-    "Frame",
-    "Group",
-    "Slice",
-    "Rectangle",
-    "Line",
-    "Arrow",
-    "Ellipse",
-    "Polygon",
-    "Star",
-    "Vector",
-    "Component"
-];
-// Stats
+const renameMsgs = ["Cleaned", "Affected", "Made it with", "Fixed", "Glued"];
+const idleMsgs = ["No new lines, already", "I see no line breaks", "Any new lines? I can't see it", "Nothing to do, your layers are great"];
+const regex = /\r?\n|\r/g;
 // Variables
 let notification;
 let selection;
 let working;
 let count = 0;
-figma.on("currentpagechange", escape);
-// For networking purposes
-figma.showUI(__html__, { visible: false });
-const post = (k, v = 1, last = false) => figma.ui.postMessage({ k: k, v: v, last: last });
-figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-    if (msg === "finished")
-        figma.closePlugin();
-    else
-        console.log(msg);
-});
+figma.on("currentpagechange", cancel);
 // Main + Elements Check
-post("started");
-const start = Date.now();
 working = true;
 selection = figma.currentPage.selection;
 console.log(selection.length + " selected");
 if (selection.length)
     for (const node of selection)
-        recursiveRename(node);
+        recursiveClean(node);
 else
-    recursiveRename(figma.currentPage);
-finish();
-function recursiveRename(node) {
-    if (node.type !== "PAGE") {
-        const match = node.name.match(regex);
-        const index = (match && match.length > 0) ? dict.indexOf(match[0]) : -1;
-        if (index >= 0) {
-            count++;
-            node.name = dict[index];
+    recursiveClean(figma.currentPage);
+function recursiveClean(node) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('recursion with ' + node.toString());
+        if (node.type === "TEXT") {
+            if (node.hasMissingFont)
+                notify("You have layers with missing fonts");
+            else
+                figma.loadFontAsync(node.fontName).then(() => {
+                    console.log(node.characters);
+                    node.characters = node.characters.replace(regex, ' ');
+                    count++;
+                    finish();
+                });
         }
-    }
-    if ("children" in node) {
-        for (const child of node.children) {
-            recursiveRename(child);
+        else if ("children" in node) {
+            for (const child of node.children) {
+                recursiveClean(child);
+            }
         }
-    }
+    });
 }
 function finish() {
     working = false;
@@ -78,25 +57,20 @@ function finish() {
         notify(confirmMsgs[Math.floor(Math.random() * confirmMsgs.length)] +
             " " + renameMsgs[Math.floor(Math.random() * renameMsgs.length)] +
             " " + ((count === 1) ? "only one layer" : (count + " layers")));
-        const time = (Date.now() - start) / 1000;
-        console.log("Renamed " + count + " layers in " + time + " seconds");
-        post("renamed", count);
-        post("runned for", time, true);
     }
     else
         notify(idleMsgs[Math.floor(Math.random() * idleMsgs.length)]);
-    // figma.closePlugin()
+    figma.closePlugin();
 }
 function notify(text) {
     if (notification != null)
         notification.cancel();
     notification = figma.notify(text);
 }
-function escape() {
+function cancel() {
     if (notification != null)
         notification.cancel();
     if (working) {
         notify("Plugin work have been interrupted");
-        post("interrupted");
     }
 }
